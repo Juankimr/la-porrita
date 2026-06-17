49378c868caad81f63203bf6611a530ca08bc87f
Juanki <juankimr@gmail.com>
2026-06-17 20:42:41 +0200

    Serve static files in development for DEBUG=True

--- porrita/urls.py
+++ porrita/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -5,3 +6,15 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pool.urls")),
 ]
+
+if settings.DEBUG:
+    from django.contrib.staticfiles import views
+    from django.urls import re_path
+    import os
+
+    def serve_static(request, path):
+        return views.serve(request, path)
+
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve_static),
+    ]
